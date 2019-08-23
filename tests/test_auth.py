107ab2125d01fea91f4967ebfc7c547eda30f89d
dev <arukinagara.kangaeru@gmail.com>
2019-08-23 04:36:50 +0000

    リフレッシュトークンについてのテストを追加

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,10 @@
 import pytest
 import json
+import datetime
+import time
 from flask import Flask, g, session
 from flask_jwt_extended import (
-    JWTManager, create_access_token, create_refresh_token,
+    JWTManager, create_access_token, create_refresh_token, jwt_required,
     jwt_refresh_token_required, get_jwt_identity, decode_token
 )
 from api.db import get_db
@@ -59,8 +61,8 @@ def test_login_validate_input(client, req_payload, res_payload):
 ))
 def test_login(app, client, req_payload):
     with app.app_context():
-        app.config['JWT_SECRET_KEY'] = 'test'
-        JWTManager(app)
+        jwt = JWTManager(app)
+
         access_token = create_access_token(req_payload['username'])
         refresh_token = create_refresh_token(req_payload['username'])
 
@@ -75,3 +77,53 @@ def test_login(app, client, req_payload):
         assert response.status_code == 200
         assert decode_token(res_access_token)['identity'] == req_payload['username']
         assert decode_token(res_refresh_token)['identity'] == req_payload['username']
+
+
+# リフレッシュトークン
+@pytest.mark.parametrize(('req_payload'), (
+    ({'username': 'test', 'password': 'test'}),
+))
+def test_refresh(app, client, req_payload):
+    with app.app_context():
+        # テスト用にアクセストークンの有効期限を1秒に設定する
+        app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(seconds=1)
+        jwt = JWTManager(app)
+
+        # 認証確認用の一時的なURI
+        @app.route('/protected', methods=['GET'])
+        @jwt_required
+        def access_protected():
+            return 'ok'
+
+        access_token = create_access_token(req_payload['username'])
+        refresh_token = create_refresh_token(req_payload['username'])
+
+        response = client.get('/protected',
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(access_token)},
+        )
+        assert response.status_code == 200
+
+        time.sleep(2)
+
+        response = client.get('/protected',
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(access_token)},
+        )
+        assert response.status_code == 401
+
+        refresh_response = client.post('/auth/refresh',
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(refresh_token)},
+        )
+
+        refresh_access_token = json.loads(refresh_response.data)['access_token']
+        response = client.get('/protected',
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer {}'.format(refresh_access_token)},
+        )
+        assert response.status_code == 200
